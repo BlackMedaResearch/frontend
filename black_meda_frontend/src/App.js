66d3064816ca9a3e2b4960eaf050a5b34f9db242
Johannes Stahr <johannesstahrofficial@gmail.com>
2023-01-22 11:32:25 +0100

    setup backround for landingpage

--- black_meda_frontend/src/App.js
+++ black_meda_frontend/src/App.js
@@ -1,9 +1,17 @@
-
+import video from "./assets/videos/backround.mp4";
+import styles from "./App.module.css";
 function App() {
   return (
-    <h1>Frontend Back Meda research</h1>
+    <div className="">
+      <video
+        src={video}
+        autoPlay
+        loop
+        muted
+        className={styles["backround-video"]}
+      ></video>
+    </div>
   );
 }
 
 export default App;
-
