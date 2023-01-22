29a9aa695e900f2d8ff2092ae3f3a56103fd9091
Johannes Stahr <johannesstahrofficial@gmail.com>
2023-01-22 21:50:48 +0100

    add landing page opener

--- black_meda_frontend/src/App.js
+++ black_meda_frontend/src/App.js
@@ -1,11 +1,13 @@
 import video from "./assets/videos/backround.mp4";
 import styles from "./App.module.css";
 import Header from "./Components/UI/header/header";
+import Opener from "./Components/Landingpage/Opener";
 function App() {
   return (
     <div className={styles["wrapper"]}>
       <Header/>
       <video className={styles['backround-video']} src={video} autoPlay loop muted/>
+      <Opener/>
 
     </div>
   );
--- black_meda_frontend/src/Components/Landingpage/Opener.js
+++ black_meda_frontend/src/Components/Landingpage/Opener.js
@@ -1,12 +1,20 @@
+import styles from "./Opener.module.css";
+
 const Opener = () => {
   return (
-    <div>
-      <h1>Tools um für die besser analyse der Wirtschaft.</h1>
-      <div>
-          <a href="">Twitter Feed</a>
-          <div>
-              <p>View Macros</p>
+    <div className={styles["wrapper-opener"]}>
+      <div className={styles.wrapper}>
+        <h1 className={styles["title-header"]}>
+          Tools um für die besser analyse der Wirtschaft.
+        </h1>
+        <div className={styles["button-box"]}>
+          <a href="" className={styles["left-button"]}>
+            Twitter Feed
+          </a>
+          <div className={styles['right-button']}>
+            <p>View Macros</p>
           </div>
+        </div>
       </div>
     </div>
   );
