d36073c66ac784ba30f8cc1c76ce2d5f4d0d5f71
Johannes Stahr <johannesstahrofficial@gmail.com>
2023-03-30 19:31:12 +0200

    impliment date util in banner

--- black_meda_frontend/src/Components/Finance News Feed/Banner.js
+++ black_meda_frontend/src/Components/Finance News Feed/Banner.js
@@ -1,35 +1,35 @@
 import { useState, useEffect } from "react";
 import Searchbar from "./Searchbar";
-import styles from "./Banner.module.css"
+import styles from "./Banner.module.css";
+import { dateString } from "./utils";
 
 const Banner = () => {
   const [ForntNewsData, setFrontNews] = useState([]);
+
   useEffect(() => {
     const fetchNewsApi = async () => {
       const url =
         "https://newsapi.org/v2/everything?" +
-        "q=Apple&" +
-        "from=2023-03-22&" +
+        "q=Bitcoin&" +
+        `from=${dateString}&` +
         "sortBy=popularity&" +
         "apiKey=ccbd98fc4cf94220849d3a584343667f";
       const response = await fetch(url);
       const data = await response.json();
-      console.log(data.articles);
-      setFrontNews(data.articles.slice(3,4));
-      console.log(data);
+      setFrontNews(data.articles.slice(8, 9));
     };
     fetchNewsApi();
-  }, []);
+  }, [dateString]);
 
   return (
     <div className={styles["wrapper-banner"]}>
       {ForntNewsData.map((item) => (
         <div className={styles["news-wrapper"]}>
-          <img src={item.urlToImage} className={styles.img}/>
-          <div className={styles.overlay}></div> 
+          <img src={item.urlToImage} className={styles.img} />
+          <div className={styles.overlay}></div>
         </div>
       ))}
-      <Searchbar/>
+      <Searchbar />
     </div>
   );
 };
