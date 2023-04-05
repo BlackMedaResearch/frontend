e6f9be9f138af7db7893a170e8c63e254f640d05
Johannes Stahr <johannesstahrofficial@gmail.com>
2023-04-05 20:46:39 +0200

    rest

--- black_meda_frontend/src/Components/Finance News Feed/News.js
+++ black_meda_frontend/src/Components/Finance News Feed/News.js
@@ -1,9 +1,13 @@
-const News = () =>{
-    return(
-        <div>
-            
-        </div>
-    )
+import styles from "./styles/News.module.css";
 
-}
-export default News
+const News = (props) => {
+  return (
+    <div className={styles["wrapper-news"]}>
+      <img src={props.imgsrc} className={styles["img-wrapper"]} />
+      <div className={styles["info-wrapper"]}>
+        <h2 className={styles["heading"]}>{props.heading}</h2>
+      </div>
+    </div>
+  );
+};
+export default News;
--- black_meda_frontend/src/Components/Finance News Feed/ResultsSearch.js
+++ black_meda_frontend/src/Components/Finance News Feed/ResultsSearch.js
@@ -1,36 +1,38 @@
-import { useEffect,useState } from "react"
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
+import News from "./News";
+import styles from "./styles/ResultsSearch.module.css"
 
-const ResultsSearch = () =>{
-    const dateString = useSelector((state) => state.search.date);
-    const pageInt = useSelector((state) => state.search.page)
-    const searchquery = encodeURIComponent(useSelector((state)=> state.search.searchquery))
-    const [results,setResults] = useState([])
-    useEffect(() => {
-        const fetchNewsApi = async () => {
-          const url =
-            "https://newsapi.org/v2/everything?" +
-            `q=${searchquery}&` +
-            `from=${dateString}&` +
-            "sortBy=popularity&" +
-            `page=${pageInt.toString()}&` +
-            "apiKey=ccbd98fc4cf94220849d3a584343667f";
-          const response = await fetch(url);
-          const data = await response.json();
-          console.log(data)
-          setResults(data.articles)
-        };
-        fetchNewsApi();
-      }, [dateString,searchquery]);
-    
+const ResultsSearch = () => {
+  const dateString = useSelector((state) => state.search.date);
+  const pageInt = useSelector((state) => state.search.page);
+  const searchquery = encodeURIComponent(
+    useSelector((state) => state.search.searchquery)
+  );
+  const [results, setResults] = useState([]);
+  useEffect(() => {
+    const fetchNewsApi = async () => {
+      const url =
+        "https://newsapi.org/v2/everything?" +
+        `q=${searchquery}&` +
+        `from=${dateString}&` +
+        "sortBy=popularity&" +
+        `page=${pageInt.toString()}&` +
+        "apiKey=ccbd98fc4cf94220849d3a584343667f";
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+      setResults(data.articles);
+    };
+    fetchNewsApi();
+  }, [dateString, searchquery]);
 
-    return(
-        <div>
-            {results.map((item)=>(
-                <p>{item.author}</p>
-            ))}
-
-        </div>
-    )
-}
-export default ResultsSearch
+  return (
+    <div className={styles["search-results-wrapper"]}>
+      {results.map((item) => (
+        <News imgsrc={item.urlToImage} heading={item.title} />
+      ))}
+    </div>
+  );
+};
+export default ResultsSearch;
