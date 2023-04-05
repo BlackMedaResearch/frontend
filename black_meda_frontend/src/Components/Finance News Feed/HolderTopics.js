f45dddb1b9f7ac476a610ae9d80c19a31c7d0022
Johannes Stahr <johannesstahrofficial@gmail.com>
2023-04-05 20:46:27 +0200

    holder topics update

--- black_meda_frontend/src/Components/Finance News Feed/HolderTopics.js
+++ black_meda_frontend/src/Components/Finance News Feed/HolderTopics.js
@@ -1,23 +1,68 @@
+import styles from "./styles/HolderTopics.module.css";
+import sec from "../../assets/videos/Finance News/sec.mp4";
+import fed from "../../assets/videos/Finance News/fed.mp4";
+import bank from "../../assets/videos/Finance News/bank.mp4";
+import crypto from "../../assets/videos/Finance News/crypto.mp4";
 
+const HolderTopics = () => {
+  return (
+    <div className={styles["wrapper-hottopics"]}>
+      <div>
+        <h1>Hot Topics</h1>
+      </div>
+      <div className={styles["holder-images"]}>
+        <a>
+          <div>
+            <video
+              className={styles["video-wrapper"]}
+              src={sec}
+              autoPlay
+              loop
+              muted
+            />
+            SEC
+          </div>
+        </a>
+        <a>
+          <div>
+            <video
+              className={styles["video-wrapper"]}
+              src={crypto}
+              autoPlay
+              loop
+              muted
+            />
+            Krypto
+          </div>
+        </a>
 
-const HolderTopics = () =>{
-    return(
-        <div>
-            <div>
-                <h1>Hot Topics</h1>
-                <div>
-                    <div>
-                        <a>SEC</a>
-                        <a>Krypto</a>
-                        <a>FED</a>
-                        <a>Bank Collaps</a>
-                    </div>
+        <a>
+          <div>
+            <video
+              className={styles["video-wrapper"]}
+              src={fed}
+              autoPlay
+              loop
+              muted
+            />
+            FED
+          </div>
+        </a>
+        <a>
+          <div>
+            <video
+              className={styles["video-wrapper"]}
+              src={bank}
+              autoPlay
+              loop
+              muted
+            />
+          </div>
+          Bank Collaps
+        </a>
+      </div>
+    </div>
+  );
+};
 
-                </div>
-            </div>
-        </div>
-
-    )
-}
-
-export default HolderTopics
+export default HolderTopics;
