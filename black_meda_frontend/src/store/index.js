982181e72761bd355c94131748f4ebafac98ed1b
Johannes Stahr <johannesstahrofficial@gmail.com>
2023-04-07 16:30:10 +0200

    add the total redux store with modal slice

--- black_meda_frontend/src/store/index.js
+++ black_meda_frontend/src/store/index.js
@@ -1,11 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import searchSlice from "./search";
+import modalSlice from "./modal";
 
 const store = configureStore({
   reducer: {
-      search:searchSlice.reducer
+      search:searchSlice.reducer,
+      modal:modalSlice.reducer
   },
 });
 
 export const searchActions = searchSlice.actions
+export const modalActions = modalSlice.actions
 export default store;
