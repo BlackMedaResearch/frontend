8d49df1b7b1701554f6f9209c4bf5fc48a52fb22
Johannes Stahr <johannesstahrofficial@gmail.com>
2023-04-07 12:08:22 +0200

    keine plan was das war

--- black_meda_frontend/src/Components/Finance News Feed/News.js
+++ black_meda_frontend/src/Components/Finance News Feed/News.js
@@ -1,11 +1,57 @@
 import styles from "./styles/News.module.css";
+import { useDispatch } from "react-redux";
+import { searchActions } from "../../store";
 
 const News = (props) => {
+  const dispatch = useDispatch();
+
+  const buttonClickHandler = () => {
+    dispatch(
+      searchActions.updateOBJ({
+        author: props.author,
+        content: props.content,
+        description: props.description,
+        publishedAt: props.publishedAt,
+        source: props.source,
+        title: props.title,
+        url: props.url,
+        urlToImage: props.urlToImage,
+      })
+    );
+    dispatch(searchActions.updateShowNewsModal(true));
+  };
   return (
     <div className={styles["wrapper-news"]}>
-      <img src={props.imgsrc} className={styles["img-wrapper"]} />
+      <img src={props.imgsrc} className={styles["img-wrapper"]} alt="newspicture" />
       <div className={styles["info-wrapper"]}>
         <h2 className={styles["heading"]}>{props.heading}</h2>
+      </div>
+      <div className={styles["wrapper-show-more"]}>
+        <button onClick={buttonClickHandler}>
+          <div className={styles["text"]}>
+            <span>zum</span>
+            <span>Artikel</span>
+          </div>
+          <div className={styles["clone"]}>
+            <span>zum</span>
+            <span>Artikel</span>
+          </div>
+          <svg
+            width="20px"
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            stroke-width="2"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M14 5l7 7m0 0l-7 7m7-7H3"
+            ></path>
+          </svg>
+        </button>
       </div>
     </div>
   );
--- black_meda_frontend/src/store/search.js
+++ black_meda_frontend/src/store/search.js
@@ -1,10 +1,22 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { act } from "react-dom/test-utils";
 import { dateString } from "./utils";
 
 const initalState = {
   searchquery: "Apple",
   date: dateString,
-  page: 1
+  newsOBJ: {
+    author: null,
+    content: null,
+    description: null,
+    publishedAt: null,
+    source: null,
+    title: null,
+    url: null,
+    urlToImage: null,
+  },
+  showNewsModal: false,
+  page: 1,
 };
 
 const searchSlice = createSlice({
@@ -14,17 +26,32 @@ const searchSlice = createSlice({
     updatesearchquery(state, action) {
       state.searchquery = action.payload;
     },
-    decreasePage(state){
-        if (state.page === 1) {
-            return
-        }
-        state.page = state.page - 1
+    decreasePage(state) {
+      if (state.page === 1) {
+        return;
+      }
+      state.page = state.page - 1;
     },
-    increasePage(state){
-        if (state.page === 5) {
-            return
-        }
-        state.page = state.page + 1
+    increasePage(state) {
+      if (state.page === 5) {
+        return;
+      }
+      state.page = state.page + 1;
+    },
+    updateOBJ(state, action) {
+      state.newsOBJ = {
+        author: action.payload.author,
+        content: action.payload.content,
+        description: action.payload.description,
+        publishedAt: action.payload.publishedAt,
+        source: action.payload.source,
+        title: action.payload.source,
+        url: action.payload.url,
+        urlToImage: action.payload.urlToImage,
+      };
+    },
+    updateShowNewsModal(state,action){
+      state.showNewsModal = action.payload
     }
   },
 });
