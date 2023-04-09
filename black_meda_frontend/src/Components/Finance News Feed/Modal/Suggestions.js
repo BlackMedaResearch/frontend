8e5a8e18e3075526923cd6b8ab2b74eb0718b2b6
Johannes Stahr <johannesstahrofficial@gmail.com>
2023-04-09 20:23:15 +0200

    setup on switch click

--- black_meda_frontend/src/Components/Finance News Feed/Modal/Suggestions.js
+++ black_meda_frontend/src/Components/Finance News Feed/Modal/Suggestions.js
@@ -1,12 +1,29 @@
 import { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { searchActions } from "../../../store";
 import styles from "./styles/Suggestions.module.css";
 
 const Suggestions = (props) => {
+  const dispatch = useDispatch();
   const dateString = useSelector((state) => state.search.date);
   const maxPage = useSelector((state) => state.search.maxPage);
   let pageInt = useSelector((state) => state.search.page);
   pageInt = maxPage >= pageInt + 1 ? pageInt + 1 : pageInt;
+
+  const onClickHandler = (item) => {
+    dispatch(
+      searchActions.updateOBJ({
+        author: item.author,
+        content: item.content,
+        description: item.description,
+        publishedAt: item.publishedAt,
+        source: item.source,
+        title: item.title,
+        url: item.url,
+        urlToImage: item.urlToImage,
+      })
+    );
+  };
 
   const searchquery = encodeURIComponent(
     useSelector((state) => state.search.searchquery)
@@ -23,7 +40,7 @@ const Suggestions = (props) => {
         "apiKey=ccbd98fc4cf94220849d3a584343667f";
       const response = await fetch(url);
       const data = await response.json();
-      console.log(data);
+      console.log(data + "Here suggestiotn");
       setResults(data.articles.slice(0, 3));
     };
     fetchNewsApi();
@@ -36,13 +53,12 @@ const Suggestions = (props) => {
       </div>
       <div className={styles["holder-suggestions"]}>
         {results.map((item) => (
-          <div>
+          <div onClick={() => onClickHandler(item)}>
             <img
               src={item.urlToImage}
               alt="alternative artikel"
               className={styles["img"]}
             />
-            <p>{item.url}</p>
           </div>
         ))}
       </div>
