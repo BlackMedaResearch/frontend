b545ccf0129a5044ff04780b5b06b299ec9e4186
Johannes Stahr <johannesstahrofficial@gmail.com>
2023-04-24 08:58:21 +0200

    settiing up footer element

--- black_meda_frontend/src/App.js
+++ black_meda_frontend/src/App.js
@@ -1,16 +1,49 @@
-import { createBrowserRouter,RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import Home from "./Pages/Landingpage/Home"
-import Twitter from "./Pages/FinanceNews/LiveFeed"
+import Home from "./Pages/Landingpage/Home";
+import News from "./Pages/FinanceNews/LiveFeed";
 import Macros from "./Pages/Macros/Macros";
 import Void from "./Pages/Void/Void";
+import Root from "./Pages/Root";
+import { Children } from "react";
 
-const routes = [{path:"/", element:<Home/>},{path:"/finance_news", element:<Twitter/>},{path:"/macro", element:<Macros/>},{path:"/void", element:<Void/>},{path:"/vc"}]
+const routes = [
+  { path: "/", element: <Home /> },
+  { path: "/finance_news", element: <News /> },
+  { path: "/macro", element: <Macros /> },
+  { path: "/void", element: <Void /> },
+  { path: "/vc" },
+];
 
-const router = createBrowserRouter(routes)
+const router = createBrowserRouter(routes);
 
+const router2 = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "/void",
+        element: <Void />,
+      },
+    ],
+  },
+  {
+    path: "/macro",
+    element: <Macros />,
+    //loader, action
+  },
+  {
+    path: "/finance_news",
+    element: <News />,
+  },
+]);
 
 function App() {
-  return <RouterProvider router={router}/>
+  return <RouterProvider router={router} />;
 }
 export default App;
