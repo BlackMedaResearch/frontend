327dd76b985e608a115b2a2bc0ce05b00664bdc2
Johannes Stahr <johannesstahrofficial@gmail.com>
2023-04-25 08:55:56 +0200

    footer start

--- black_meda_frontend/src/Components/UI/footer/Footer.js
+++ black_meda_frontend/src/Components/UI/footer/Footer.js
@@ -1,1 +1,24 @@
-const 
+import styles from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <footer className={styles["wrapper-footer"]}>
+      <div className={styles["footer-box"]}>
+        <div></div>
+        <div>
+          <div>
+              
+          </div>
+          <div>
+              <div>
+
+              </div>
+          </div>
+        </div>
+        <div></div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
